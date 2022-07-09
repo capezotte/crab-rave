d13e8cbf7a013f17c7a4c8bfa406849ac929d17a
capezotte <carana2099@gmail.com>
2022-07-09 19:30:00 -0300

    add howto, code cleanup

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
 # crab rave
 
-send him `/cu` and he'll spill out nonsense
+Send him `/cu` and he'll spill out nonsense.
 
-requires sqlite
+Require Sqlite3 and Rust >= 1.61.
+
+# DIY
+
+Set the `TELOXIDE_TOKEN` environment variable to your Telegram bot token.
+
+Upon the first run, the bot will look for a file called `in.txt`, using the following format:
+
+- Lines starting with an empty space, containing a single word, are words that can begin a sentence.
+- Lines not starting with an empty space, contaning a single word, are words that can end a sentence.
+- Lines not starting with an empty space, containing two words, are a word pair.
+
+For example, the sentence `blazing fast memory safe nonsense generator` will generate:
+
+```plain
+ blazing
+blazing fast
+fast memory
+memory safe
+safe nonsense
+nonsense generator
+generator
+```
+
+For convenience, you can run a line-by-line chat dump through the `mkdb` shell script to generate files like the above automatically.
+
+It'll be converted to an indexed sqlite database stored under `chain.sqlite`.
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,8 @@ use std::error::Error;
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
-    log::info!("Starting command bot...");
-
     prep_markov();
+    log::info!("Done. Starting bot.");
     let bot = Bot::from_env().auto_send();
     
     teloxide::commands_repl(bot, answer, Command::ty()).await;
@@ -25,11 +24,13 @@ async fn answer(
     message: Message,
     command: Command,
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let Command::Cu(s) = command;
+    let min_len: u8 = s.parse().unwrap_or(5);
 
-    if let Some(reply) = markov(5, None) {
+    if let Some(reply) = markov(min_len, None) {
         bot.send_message(message.chat.id, reply).await?;
     } else {
-        bot.send_message(message.chat.id, format!("Can't find '{}' in database.", "s")).reply_to_message_id(message.id).await?;
+        bot.send_message(message.chat.id, "unable to generate message!1! panic!").reply_to_message_id(message.id).await?;
     }
 
     Ok(())
@@ -37,8 +38,8 @@ async fn answer(
 
 fn prep_markov() {
     // Check if we've already init
-    log::info!("Readying markov chain...");
-    if let Ok(connection) = sqlite::open("./wmbr.sqlite") {
+    log::trace!("Readying markov chain...");
+    if let Ok(connection) = sqlite::open("./markov.sqlite") {
         let mut found = false;
         match connection.iterate("SELECT name FROM sqlite_master WHERE name = 'pairs'", |_| { found = true; false }) {
             Ok(()) | Err( sqlite::Error { code: Some(4), .. } ) => {
@@ -51,7 +52,7 @@ fn prep_markov() {
         }
     }
     // Else init
-    log::info!("Initting markov...");
+    log::trace!("Initting markov...");
     let connection = sqlite::open(":memory:").unwrap();
 
     // Initialize
@@ -60,37 +61,44 @@ fn prep_markov() {
         .execute("CREATE TABLE pairs (word1 TEXT NOT NULL, word2 TEXT);").unwrap(); // pairs. If second word is null, ending.
     let mut begin_stmt = connection.prepare("INSERT INTO begin VALUES (?);").unwrap();
     let mut add_stmt = connection.prepare("INSERT INTO pairs VALUES (?, ?);").unwrap();
-    for line in fs::read_to_string("wmbr.db").expect("Can't open db").lines() {
-        println!("reading: {line}");
+    for line in fs::read_to_string("in.txt").expect("Can't open input").lines() {
         if line.starts_with(" ") {
-            begin_stmt.bind(1, line.trim()).unwrap();
+            let new_word = line.trim();
+            begin_stmt.bind(1, new_word).unwrap();
             begin_stmt.next().unwrap();
             begin_stmt.reset().unwrap();
+            log::trace!("Added word: {new_word}");
         } else {
             let mut split_str = line.split_whitespace();
+            let word1 = split_str.next().unwrap_or("");
+            let word2 = split_str.next();
+            if let Some(w2) = word2 {
+                log::trace!("Word pair: {word1} and {w2}");
+            } else {
+                log::trace!("End word: {word1}");
+            }
             add_stmt.bind(1, split_str.next().unwrap_or("")).unwrap();
             add_stmt.bind(2, split_str.next()).unwrap();
             add_stmt.next().unwrap();
             add_stmt.reset().unwrap();
         }
     }
-    log::info!("Create index...");
+    log::trace!("Create index...");
     connection.execute("CREATE INDEX idx_word1 ON pairs (word1);").unwrap();
-    log::info!("Saving markov to disk...");
-    connection.execute("VACUUM INTO './wmbr.sqlite';").unwrap();
-    log::info!("Done. Starting bot.");
+    log::trace!("Saving markov to disk...");
+    connection.execute("VACUUM INTO './markov.sqlite';").unwrap();
 }
 
-fn markov(len: i32, starting: Option<&str>) -> Option<String> {
+fn markov(len: u8, starting: Option<&str>) -> Option<String> {
     use sqlite::State;
-    let connection = sqlite::open("./wmbr.sqlite").unwrap();
+    let connection = sqlite::open("./markov.sqlite").unwrap();
 
     let mut cur_word = if let Some(init) = starting {
         init.to_owned()
     } else {
         let mut begin_stmt = connection.prepare("SELECT * FROM begin ORDER BY random();").unwrap();
         if begin_stmt.next().unwrap() == State::Done {
-            panic!("Unitialized DB");
+            panic!("Bad DB: can't select from begin table");
         }
         begin_stmt.read::<String>(0).unwrap()
     };
@@ -99,15 +107,14 @@ fn markov(len: i32, starting: Option<&str>) -> Option<String> {
     // Fill up most words
     {
         let mut next_word_stmt = connection.prepare("SELECT word2 FROM pairs WHERE word1 = ? AND word2 IS NOT NULL ORDER BY random();").unwrap();
-        let mut i = len;
-        while i > 0 {
+        for i in 0..len {
+            log::trace!("Iteration {i} without null");
             next_word_stmt.bind(1, cur_word.as_str()).unwrap();
             next_word_stmt.next().unwrap();
             if let Some(new_word) = next_word_stmt.read::<Option<String>>(0).unwrap() {
                 ret.push_str(" ");
                 ret.push_str(&new_word);
                 cur_word = new_word.clone();
-                i -= 1;
             } else {
                 break;
             }
@@ -117,15 +124,21 @@ fn markov(len: i32, starting: Option<&str>) -> Option<String> {
     // Final words
     { 
         let mut final_word_stmt = connection.prepare("SELECT word2 FROM pairs WHERE word1 = ? ORDER BY random();").unwrap();
-        final_word_stmt.bind(1, cur_word.as_str()).unwrap();
-        final_word_stmt.next().unwrap();
-        while let Some(new_word) = final_word_stmt.read::<Option<String>>(0).unwrap() {
-            ret.push_str(" ");
-            ret.push_str(&new_word);
-            cur_word = new_word.clone();
-            final_word_stmt.reset().unwrap();
+        loop {
+            log::trace!("Extra iteration null");
             final_word_stmt.bind(1, cur_word.as_str()).unwrap();
             final_word_stmt.next().unwrap();
+            match final_word_stmt.read::<Option<String>>(0).unwrap() {
+                Some(new_word) => {
+                    ret.push_str(" ");
+                    ret.push_str(&new_word);
+                    cur_word = new_word.clone();
+                },
+                None => {
+                    break;
+                }
+            }
+            final_word_stmt.reset().unwrap();
         }
     }
 
